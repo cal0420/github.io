eb35cca24589bfb083c820dde7b0bca692e5ccbb
cal0420 <cal420vr@gmail.com>
2026-07-29 18:00:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const DISCORD_INVITE_URL = "https://discord.gg/c4ghub";
 
 document.querySelectorAll("[data-discord]").forEach(link => {
-    link.addEventListener("click", () => {
-        link.href = DISCORD_INVITE_URL;
-    });
+    link.href = DISCORD_INVITE_URL;
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
 });
