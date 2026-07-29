d8f36792d005bdd5f8dded11272f1db3a1ef0340
cal0420 <cal420vr@gmail.com>
2026-07-29 18:13:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const DISCORD_INVITE_URL = "https://discord.gg/c4ghub";
 
-document.querySelectorAll("[data-discord]").forEach(link => {
+document.querySelectorAll("[data-discord]").forEach((link) => {
     link.href = DISCORD_INVITE_URL;
     link.target = "_blank";
     link.rel = "noopener noreferrer";
